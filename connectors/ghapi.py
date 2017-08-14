9a546379f4f26de30513c277516b8286c42539a4
Syed Abdullah <contact@syedabdullah.net>
2017-08-14 15:16:32 +0800

    api: add custom API for inviting users to organisation

---
+++ connectors/ghapi.py
@@ -0,0 +1,38 @@
+import requests
+
+import logging
+
+class GHAPI:
+    """Class with static methods for calling up API"""
+    BASE_URL = 'https://api.github.com'
+
+    @classmethod
+    def handle_api_exception(cls, request):
+        json_resp = None
+
+        if request is not None:
+            try:
+                json_resp = request.json()
+            except ValueError as e:
+                json_Resp = None
+
+        logging.error('Exception raised by API! Response: %s', json_resp)
+
+    @classmethod
+    def invite_user(cls, api_key, organisation, username):
+        """Invites a particular user to an organisation"""
+        url = '{}/orgs/{}/memberships/{}'.format(cls.BASE_URL, organisation, username)
+        auth_tuple = ('token', api_key)
+        r = None
+        try:
+            r = requests.get(url, auth=auth_tuple)
+        except requests.exceptions.RequestException as e:
+            cls.handle_api_exception(None)
+            return False
+
+        try:
+            r.raise_for_status()
+            return True
+        except requests.exceptions.HTTPError as e:
+            cls.handle_api_exception(r)
+            return False
